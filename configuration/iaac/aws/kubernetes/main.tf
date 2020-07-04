d5e0d0764fda5e970da0e91961373ce8c0b9a8fd
Gururaj <grpandurangi@gmail.com>
2020-07-04 20:07:16 +0530

    first commit

--- configuration/iaac/aws/kubernetes/main.tf
+++ configuration/iaac/aws/kubernetes/main.tf
@@ -33,6 +33,11 @@ locals {
   cluster_name = "test-eks-${random_string.suffix.result}"
 }
 
+resource "random_string" "suffix" {
+  length  = 8
+  special = false
+}
+
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "2.6.0"
