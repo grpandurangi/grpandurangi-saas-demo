10f900399c79a8ac18e8663f903404a8d740d8b3
Gururaj <grpandurangi@gmail.com>
2020-07-05 07:52:15 +0530

    added sec group

--- configuration/iaac/vpc/main.tf
+++ configuration/iaac/vpc/main.tf
@@ -13,10 +13,10 @@ provider "aws" {
 data "aws_availability_zones" "available" {
 }
 
-#data "aws_security_group" "default" {
-#  name   = "default"
-#  vpc_id = module.vpc.vpc_id
-#}
+data "aws_security_group" "default" {
+  name   = "default"
+  vpc_id = module.vpc.vpc_id
+}
 
 module "vpc" {
 
