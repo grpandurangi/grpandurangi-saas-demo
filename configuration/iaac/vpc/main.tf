dcafbc328baa754f6f212b9e3d7881be0b886fd7
Gururaj <grpandurangi@gmail.com>
2020-07-05 06:47:28 +0530

    added vpc

---
+++ configuration/iaac/vpc/main.tf
@@ -0,0 +1,47 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+data "aws_availability_zones" "available" {
+}
+
+data "aws_security_group" "default" {
+  name   = "default"
+  vpc_id = module.vpc.vpc_id
+}
+
+module "vpc" {
+  source = "../../"
+
+  name = "complete-example"
+
+  cidr = "20.10.0.0/16" # 10.0.0.0/8 is reserved for EC2-Classic
+
+  #azs                 = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  azs                  = data.aws_availability_zones.available.names
+  private_subnets     = ["20.10.1.0/24", "20.10.2.0/24", "20.10.3.0/24"]
+  public_subnets      = ["20.10.11.0/24", "20.10.12.0/24", "20.10.13.0/24"]
+  database_subnets    = ["20.10.21.0/24", "20.10.22.0/24", "20.10.23.0/24"]
+
+  create_database_subnet_group = true
+
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+  enable_dhcp_options              = true
+  dhcp_options_domain_name         = "service.consul"
+  dhcp_options_domain_name_servers = ["127.0.0.1", "10.10.0.2"]
+
+  create_flow_log_cloudwatch_iam_role  = true
+  flow_log_max_aggregation_interval    = 60
+
+  tags = {
+    Owner       = "user"
+    Environment = "staging"
+    Name        = "complete"
+  }
+
+}
