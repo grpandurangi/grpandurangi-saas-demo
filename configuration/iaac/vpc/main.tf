7ab4601b94b75f668fb95b10da3bf842312b030a
Gururaj <grpandurangi@gmail.com>
2020-07-05 07:29:07 +0530

    added nat

--- configuration/iaac/vpc/main.tf
+++ configuration/iaac/vpc/main.tf
@@ -37,8 +37,8 @@ module "vpc" {
   enable_dns_hostnames = true
   enable_dns_support   = true
 
-#  enable_nat_gateway = true
-#  single_nat_gateway = true
+  enable_nat_gateway = true
+  single_nat_gateway = true
 
 #  enable_dhcp_options              = true
 
