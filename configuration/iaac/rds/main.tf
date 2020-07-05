de829b320b0b9f11f642664839d7ce734c5eb635
Gururaj <grpandurangi@gmail.com>
2020-07-05 09:46:50 +0530

    added backend

--- configuration/iaac/rds/main.tf
+++ configuration/iaac/rds/main.tf
@@ -1,3 +1,11 @@
+terraform {
+  backend "s3" {
+    bucket = "mybucket" # Will be overridden from build
+    key    = "path/to/my/key" # Will be overridden from build
+    region = "us-east-1"
+  }
+}
+
 provider "aws" {
   region = "us-east-1"
 }
