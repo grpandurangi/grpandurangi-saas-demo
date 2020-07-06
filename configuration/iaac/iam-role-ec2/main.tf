0894d0ddc4cdf2b0381391a6d0a3bd925728faba
Gururaj <grpandurangi@gmail.com>
2020-07-06 15:20:10 +0530

    added iam role

--- configuration/iaac/iam-role-ec2/main.tf
+++ configuration/iaac/iam-role-ec2/main.tf
@@ -1,21 +1,33 @@
+terraform {
+  backend "s3" {
+    bucket = "mybucket" # Will be overridden from build
+    key    = "path/to/my/key" # Will be overridden from build
+    region = "us-east-1"
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
 resource "aws_iam_role" "ec2_s3_access_role" {
   name               = "s3-role"
   assume_role_policy = "${file("assumerolepolicy.json")}"
 }
 
 resource "aws_iam_policy" "policy" {
-  name        = "test-policy"
-  description = "A test policy"
+  name        = "saas-s3-policy"
+  description = "IAM policy for S3"
   policy      = "${file("policys3bucket.json")}"
 }
 
-resource "aws_iam_policy_attachment" "test-attach" {
-  name       = "test-attachment"
+resource "aws_iam_policy_attachment" "saas-iam-role-policy-attach" {
+  name       = "saas-iam-role-policy-attach"
   roles      = ["${aws_iam_role.ec2_s3_access_role.name}"]
   policy_arn = "${aws_iam_policy.policy.arn}"
 }
 
-resource "aws_iam_instance_profile" "test_profile" {
-  name  = "test_profile"
+resource "aws_iam_instance_profile" "iam_ec2_instance_profile" {
+  name  = "iam_ec2_instance_profile"
   role = "${aws_iam_role.ec2_s3_access_role.name}"
 }
